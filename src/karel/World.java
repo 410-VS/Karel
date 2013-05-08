3ba6100435a0ea828b2f53f590bbac136202ff4e
Noel Alexander <noel.alexander@gmail.com>
2013-05-07 21:47:20 -0400

    more theme work

--- src/karel/World.java
+++ src/karel/World.java
@@ -668,6 +668,9 @@ public class World extends JPanel
             }
             
             Home.setNewImage(newTheme+"Home.png");
+            karel.setNewTheme(newTheme);
+            //the player setnewtheme only happens when the player changes direction..
+            //need to reset the image outside of the change direction...
         }
 
 }
